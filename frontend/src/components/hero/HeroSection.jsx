cb29dfb4658133802347f79a4166e272e34cbec7
Collins Mwiti <collinsmwiti98@gmail.com>
2025-11-18 17:34:54 +0300

    last change

--- frontend/src/components/hero/HeroSection.jsx
+++ frontend/src/components/hero/HeroSection.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import { FaShippingFast, FaCertificate, FaGift } from "react-icons/fa";
 import Button from "../button/button.jsx";
 import heroBottle from "../../assets/spray.png";
-import noseknowsLogo from "../../assets/noseknows_logo.png";
+import noseknowsLogo from "../../assets/Noseknows_logo.png";
 
 const HeroSection = () => {
   // Define the navigation handlers
